ff31e1f33a82ce14b6be1776611f1745087eda22
Tseng <tseng7510@gmail.com>
2024-07-30 17:41:03 +0800

    更新

--- faq.php
+++ faq.php
@@ -1,97 +1,100 @@
 <!DOCTYPE html>
 <html lang="zh-Hant" class="no-js">
+
 <head>
-    <meta charset="UTF-8" />
-    <link rel="alternate" hreflang="x-default" href="index.php" />
-    <link rel="alternate" hreflang="zh" href="index.php" />
-    <link rel="alternate" hreflang="zh-TW" href="index.php" />
-                
-    <meta name="copyright" content="Auto Strong" />
-    <title>Auto Strong</title>
-    <?php include('include_head.php')?>
-    <link rel="stylesheet" href="css/faq.css">
+  <meta charset="UTF-8" />
+  <link rel="alternate" hreflang="x-default" href="index.php" />
+  <link rel="alternate" hreflang="zh" href="index.php" />
+  <link rel="alternate" hreflang="zh-TW" href="index.php" />
+
+  <meta name="copyright" content="Auto Strong" />
+  <title>Auto Strong</title>
+  <?php include('include_head.php') ?>
+  <link rel="stylesheet" href="css/faq.css">
 </head>
+
 <body>
-    <div class="wrapper wrapper-content">
-        <?php include('include_header.php')?>
-        <div class="banner">
-            <div class="container">
-                <div class="box">
-                    <div class="text wow fadeInLeft" data-wow-delay="0s">
-                        <h1 class="banner-title">FAQ</h1>
-                        <h2 class="banner-sub-title">FREQUENTLY ASKED QUESTIONS</h2>
-                    </div>
-                </div>
-            </div>
+  <div class="wrapper wrapper-content">
+    <?php include('include_header.php') ?>
+    <div class="banner">
+      <div class="container">
+        <div class="box">
+          <div class="text wow fadeInLeft" data-wow-delay="0s">
+            <h1 class="banner-title">FAQ</h1>
+            <h2 class="banner-sub-title">FREQUENTLY ASKED QUESTIONS</h2>
+          </div>
         </div>
-        <section class="sect sect1">
-            <div class="container">
-                <div class="faq-box">
-                    <div class="item wow fadeIn" data-wow-delay="0s">
-                        <div class="box">
-                            <div class="pic">
-                                <img src="images/faq/pic1.jpg" alt="">
-                            </div>
-                            <div class="text">
-                                <h2 class="name">Chuck <br>FAQ and Installation</h2>
-                                <span class="tag">Learn more  >></span>
-                            </div>
-                            <a href="faq_list.php" class="cover" title="Chuck FAQ and Installation" alt="Chuck FAQ and Installation">Chuck FAQ and Installation</a>
-                        </div>
-                    </div>
-
-                    <div class="item wow fadeIn" data-wow-delay="0.2s">
-                        <div class="box">
-                            <div class="pic">
-                                <img src="images/faq/pic2.jpg" alt="">
-                            </div>
-                            <div class="text">
-                                <h2 class="name">Hydraulic Cylinder FAQ and Installation</h2>
-                                <span class="tag">Learn more  >></span>
-                            </div>
-                            <a href="faq_list.php" class="cover" title="Chuck FAQ and Installation" alt="Chuck FAQ and Installation">Chuck FAQ and Installation</a>
-                        </div>
-                    </div>
-
-                    <div class="item wow fadeIn" data-wow-delay="0.4s">
-                        <div class="box">
-                            <div class="pic">
-                                <img src="images/faq/pic3.jpg" alt="">
-                            </div>
-                            <div class="text">
-                                <h2 class="name">The Instruction of Jaw Boring Rings</h2>
-                                <span class="tag">Learn more  >></span>
-                            </div>
-                            <a href="faq_list.php" class="cover" title="Chuck FAQ and Installation" alt="Chuck FAQ and Installation">Chuck FAQ and Installation</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-
-        <div class="breadcrumb-box">
-            <div class="container">
-                <ol class="breadcrumb list-inline" itemscope="" itemtype="https://schema.org/BreadcrumbList">
-                    <li itemprop="itemListElement" itemscope="" itemtype="https://schema.org/ListItem">
-                        <a itemprop="item" href="index.php">
-                        <span itemprop="name">Home</span></a>
-                        <meta itemprop="position" content="1">
-                    </li>
-                    <li itemprop="itemListElement" itemscope="" itemtype="https://schema.org/ListItem">
-                        <a itemprop="item" href="faq.php">
-                        <span itemprop="name">Support</span></a>
-                        <meta itemprop="position" content="2">
-                    </li>
-                     <li itemprop="itemListElement" itemscope="" itemtype="https://schema.org/ListItem">
-                        <a itemprop="item" href="faq.php">
-                        <span itemprop="name">FAQ</span></a>
-                        <meta itemprop="position" content="3">
-                    </li>
-                </ol>
-            </div>
-        </div>
-        <?php include('include_footer.php')?>
+      </div>
     </div>
-    <?php include('include_body_bottom.php')?>
+    <section class="sect sect1">
+      <div class="container">
+        <div class="faq-box">
+          <div class="item wow fadeIn" data-wow-delay="0s">
+            <a href="faq_list.php" class="cover" title="Chuck FAQ and Installation" alt="Chuck FAQ and Installation">Chuck FAQ and Installation</a>
+            <div class="box">
+              <div class="pic">
+                <img src="images/faq/pic1.jpg" alt="">
+              </div>
+              <div class="text">
+                <h2 class="name">Chuck <br>FAQ and Installation</h2>
+                <span class="tag">Learn more >></span>
+              </div>
+            </div>
+          </div>
+
+          <div class="item wow fadeIn" data-wow-delay="0.2s">
+            <a href="faq_list.php" class="cover" title="Chuck FAQ and Installation" alt="Chuck FAQ and Installation">Chuck FAQ and Installation</a>
+            <div class="box">
+              <div class="pic">
+                <img src="images/faq/pic2.jpg" alt="">
+              </div>
+              <div class="text">
+                <h2 class="name">Hydraulic Cylinder FAQ and Installation</h2>
+                <span class="tag">Learn more >></span>
+              </div>
+            </div>
+          </div>
+
+          <div class="item wow fadeIn" data-wow-delay="0.4s">
+            <a href="faq_list.php" class="cover" title="Chuck FAQ and Installation" alt="Chuck FAQ and Installation">Chuck FAQ and Installation</a>
+            <div class="box">
+              <div class="pic">
+                <img src="images/faq/pic3.jpg" alt="">
+              </div>
+              <div class="text">
+                <h2 class="name">The Instruction of Jaw Boring Rings</h2>
+                <span class="tag">Learn more >></span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <div class="breadcrumb-box">
+      <div class="container">
+        <ol class="breadcrumb list-inline" itemscope="" itemtype="https://schema.org/BreadcrumbList">
+          <li itemprop="itemListElement" itemscope="" itemtype="https://schema.org/ListItem">
+            <a itemprop="item" href="index.php">
+              <span itemprop="name">Home</span></a>
+            <meta itemprop="position" content="1">
+          </li>
+          <li itemprop="itemListElement" itemscope="" itemtype="https://schema.org/ListItem">
+            <a itemprop="item" href="faq.php">
+              <span itemprop="name">Support</span></a>
+            <meta itemprop="position" content="2">
+          </li>
+          <li itemprop="itemListElement" itemscope="" itemtype="https://schema.org/ListItem">
+            <a itemprop="item" href="faq.php">
+              <span itemprop="name">FAQ</span></a>
+            <meta itemprop="position" content="3">
+          </li>
+        </ol>
+      </div>
+    </div>
+    <?php include('include_footer.php') ?>
+  </div>
+  <?php include('include_body_bottom.php') ?>
 </body>
+
 </html>
